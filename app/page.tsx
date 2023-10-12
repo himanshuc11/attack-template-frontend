54e97d611b9d7a6dd3509176d4db51cb6267f938
himanshuc11 <himanshuchhatpar@gmail.com>
2023-10-12 11:35:33 +0530

    refactor: removed comments

--- app/page.tsx
+++ app/page.tsx
@@ -12,15 +12,15 @@ const todoProps = {
 export default function Home() {
   const { data: session } = useSession();
 
-  // if (session && session.user) {
-  //   return (
-  //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  //       <Button variant={"destructive"} onClick={() => signOut()}>
-  //         Sign Out
-  //       </Button>
-  //     </main>
-  //   );
-  // }
+  if (session && session.user) {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Button variant={"destructive"} onClick={() => signOut()}>
+          Sign Out
+        </Button>
+      </main>
+    );
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
