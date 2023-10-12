6cd6dd7dbcb0e71ef06e08edddc9a9bfc9bbe98a
himanshuc11 <himanshuchhatpar@gmail.com>
2023-10-12 10:58:03 +0530

    feat: Added todo sounds

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useSession, signOut, signIn } from "next-auth/react";
+import Todo from "@/components/custom/Todo";
+
+const todoProps = {
+  text: "Build Animated strike",
+  isCompleted: false,
+  id: "uuid",
+};
 
 export default function Home() {
   const { data: session } = useSession();
 
-  if (session && session.user) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Button variant={"destructive"} onClick={() => signOut()}>
-          Sign Out
-        </Button>
-      </main>
-    );
-  }
+  // if (session && session.user) {
+  //   return (
+  //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //       <Button variant={"destructive"} onClick={() => signOut()}>
+  //         Sign Out
+  //       </Button>
+  //     </main>
+  //   );
+  // }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Todo {...todoProps} />
       <Button onClick={() => signIn()}>Sign In</Button>
     </main>
   );
